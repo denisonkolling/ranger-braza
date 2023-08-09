ad49ac1ced89c3aaa0c625c985affd9922d3be83
Denison Kolling <denisonkolling@gmail.com>
2023-08-09 16:09:05 -0300

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import './App.css'
+import Navbar from "./components/Navbar"
+
 
 function App() {
   
 
   return (
     <>
+    <Navbar></Navbar>
       <h1>Ranger Braza Burguer</h1>
     </>
   )
